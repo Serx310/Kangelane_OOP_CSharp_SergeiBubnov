40d63c4e4455ea7241ea2c27bf207cdd9d19656b
Serx310 <sergeibubnov120@gmail.com>
2021-10-06 18:01:38 +0300

    Kangelane OOP

---
+++ OOP_Kangelane/Kangelane.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Kangelane
+{
+    public class Kangelane
+    {
+        protected string _nimi;
+        protected string _linn;
+        public Kangelane(string nimi, string linn)
+        {
+            _nimi = nimi;
+            _linn = linn;
+        }
+
+
+        virtual public int Päästa(int ohus)
+        {
+            return Convert.ToInt32(ohus*0.95);
+        }
+
+        virtual public string toString()
+        {
+            return _nimi.ToString();
+        }
+
+        //Getter
+        public string GetName() { return _nimi; }
+        public string GetCity() { return _linn; }
+        //Setter
+        public void SetX(string uusNimi) { _nimi = uusNimi; }
+        public void SetY(string uusLinn) { _linn = uusLinn; }
+    }
+}
---
+++ OOP_Kangelane/Superkangelane.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Kangelane
+{
+    public class Superkangelane : Kangelane
+    {
+        private double _osavus;
+
+        public Superkangelane(string nimi, string linn, double osavus) : base(nimi, linn)
+        {
+            Random r = new Random();
+            osavus = r.Next(1, 5);
+            _osavus = osavus;
+        }
+
+        public override int Päästa(int ohus)
+        {
+            int päästetud = ohus * ((95+Convert.ToInt32(_osavus))/100);
+            return päästetud;
+        }
+
+        public override string toString()
+        {
+            return $"{_nimi}, {_osavus}";
+        }
+    }
+}
